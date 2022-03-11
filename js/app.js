9dcb691ad9e54577d79a5254321412d050725779
unknown <okparajacob@gmail.com>
2022-03-11 15:35:08 +0000

    first comment

---
+++ js/app.js
@@ -0,0 +1,93 @@
+// operators
+let a = 10;
+let b = 15;
+let c = "10"
+
+// basic operator
+console.log(a + b);
+console.log(a * b);
+console.log(a - b);
+console.log(a / b);
+
+// remainder
+console.log(a % b);
+
+//urinary operator
+console.log(a++);
+console.log(a);
+console.log(a--);
+console.log(++a);
+console.log(a);
+console.log(--a);
+
+// comparison
+console.log(a < b);
+console.log(a > b);
+console.log(a == b);
+console.log(a === c);
+
+// control flow
+// if ( condition)
+
+let score = 80;
+
+if ( score >= 80) {
+    console.log("You had A")
+}   else if (score >= 75) {
+    console.log("You had B+");
+} 
+
+
+
+
+
+// switch case
+
+const gender = "Male"
+switch ( gender) {
+ case "Male":
+     console.log ("go to the washroom");
+     break;
+     case "Female":
+     console.log ("go to the female washroom");
+     break;
+     default:
+         conslole.log("no washroom fo you.")
+         break;
+
+}    
+
+// for ( let i = 1; i < 10; i++)
+// functions
+for ( let count = 1; count < 10; count ++ ) {
+    console.log(count);
+}
+
+
+
+// while - loop
+
+let count = 1;
+
+while (count < 10) {
+    console.log(count);
+    count++;
+    
+}
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
